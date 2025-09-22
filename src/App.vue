361f9261747b74ec06677b74fa779bc36751f1c3
deming-tian <136963391+deming-tian@users.noreply.github.com>
2025-09-22 09:51:08 +0800

    Update App.vue
    22

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <a href="https://vuejs.org/" target="_blank">
             <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
         </a>
-        <h1>大伟聊前端222</h1>
+        <h1>大伟聊前端22</h1>
         <chartPie :series-data="dataList" :extra-option="extraOption" />
     </div>
     <!-- <HelloWorld msg="Vite + Vue" /> -->
